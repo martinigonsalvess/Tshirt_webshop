0eb86f9b2f595139e78011961b5ecf546bd4d455
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-06-25 15:38:21 +0200

    added images

--- src/components/Products/Product/Product.js
+++ src/components/Products/Product/Product.js
@@ -8,11 +8,17 @@ import {
   IconButton,
 } from "@material-ui/core";
 import { AddShoppingCart } from "@material-ui/icons";
+import useStyles from "./styles";
 
 const Product = ({ product }) => {
+  const classes = useStyles();
   return (
     <Card className={classes.root}>
-      <CardMedia className={classes.media} image="" title={product.name} />
+      <CardMedia
+        className={classes.media}
+        image={product.image}
+        title={product.name}
+      />
       <CardContent>
         <div className={classes.CardContent}>
           <Typography variant="h5" gutterBottom>
@@ -20,7 +26,7 @@ const Product = ({ product }) => {
           </Typography>
           <Typography variant="h5">{product.price}</Typography>
         </div>
-        <Typography variant="h2" color="textSecondary">
+        <Typography variant="body2" color="textSecondary">
           {product.description}
         </Typography>
       </CardContent>
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,9 +1,25 @@
 import React from "react";
-import Grid from "@material-ui/core";
+import { Grid } from "@material-ui/core";
+
+import Product from "./Product/Product";
 
 const products = [
-  { id: 1, name: "Shoes", description: "Running shoes", price: "$5" },
-  { id: 2, name: "MacBook", description: "Apple macbook", price: "$6" },
+  {
+    id: 1,
+    name: "Shoes",
+    description: "Running shoes",
+    price: "$5",
+    image:
+      "https://cdn.vox-cdn.com/thumbor/6_tZq0BoeaVG1_yVI1ly29bCMVE=/0x0:2048x1280/920x613/filters:focal(861x477:1187x803):format(webp)/cdn.vox-cdn.com/uploads/chorus_image/image/69045732/Exbfpl2WgAAQkl8_resized.0.jpeg",
+  },
+  {
+    id: 2,
+    name: "MacBook",
+    description: "Apple macbook",
+    price: "$6",
+    image:
+      "https://assets.mmsrg.com/isr/166325/c1/-/ASSET_MMS_78976645/fee_786_587_png/APPLE-MacBook-Air-13.3-%282020%29---Zilver-M1-512GB-8GB",
+  },
 ];
 
 const Products = () => {
