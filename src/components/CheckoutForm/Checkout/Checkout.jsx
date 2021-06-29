e304a88c30258a15b2c013a110c0c12dc2bb6073
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-06-29 21:16:25 +0200

    creating funcional form, based on steps

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Paper,
   Stepper,
@@ -9,9 +9,44 @@ import {
   Divider,
   Button,
 } from "@material-ui/core";
+import useStyles from "./styles";
+
+const steps = ["Shipping address", "Payment details"];
 
 const Checkout = () => {
-  return <div></div>;
+  const [activeStep, setActiveStep] = useState(0);
+  const classes = useStyles();
+
+  //Depending on which step we currently on
+  const Form = ()=>(
+
+    
+
+
+
+
+
+  )
+  return (
+    <>
+      <div className={classes.toolbar} />
+      <main className={classes.layout}>
+        <Paper className={classes.paper}>
+          <Typography variant="h4" align="center">
+            Checkout
+          </Typography>
+
+          <Stepper activeStep={activeStep} className={classes.stepper}>
+            {steps.map((step) => (
+              <Step key={step}>
+                <StepLabel>{step}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+        </Paper>
+      </main>
+    </>
+  );
 };
 
 export default Checkout;
