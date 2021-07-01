bdc8bf6215ce978acde64c6270a216560cef0eb7
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-07-01 22:06:54 +0200

    creating buttons

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -8,26 +8,38 @@ import {
   Typography,
 } from "@material-ui/core";
 import { useForm, FormProvider } from "react-hook-form";
+import { Link } from "react-router-dom";
+import FormInput from "./CustomTextField";
 
 const AddressForm = () => {
   const methods = useForm();
+
   return (
     <>
       <Typography variant="h6" gutterBottom>
         Shipping Address
       </Typography>
-      <FormProvider{...methods}>
-        <form onSubmit={}>
+      <FormProvider {...methods}>
+        <form onSubmit="">
           <Grid container spacing={3}>
-            
-          
-          
+            <FormInput required name="firstName" label="First name" />
+            <FormInput required name="lastName" label="Last name" />
+            <FormInput required name="address" label="Address" />
+            <FormInput required name="postcode" label="Postcode" />
+            <FormInput required name="city" label="City" />
+            <FormInput required name="email" label="Email" />
           </Grid>
-        
+          <br />
+          <div style={{ display: "flex", justifyContent: "space-around" }}>
+            <Button component={Link} to="/cart" variant="outlined">
+              Back to Cart
+            </Button>
+            <Button type="submit" variant="contained" color="primary">
+              Next
+            </Button>
+          </div>
         </form>
       </FormProvider>
-
-
     </>
   );
 };
