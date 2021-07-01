525be673bc1e1de6e2d78efe8123e22025ec709b
martinigonsalves <32884454+martinigonsalves@users.noreply.github.com>
2021-07-01 10:47:50 +0200

    creating CustomTextField

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -16,6 +16,18 @@ const AddressForm = () => {
       <Typography variant="h6" gutterBottom>
         Shipping Address
       </Typography>
+      <FormProvider{...methods}>
+        <form onSubmit={}>
+          <Grid container spacing={3}>
+            
+          
+          
+          </Grid>
+        
+        </form>
+      </FormProvider>
+
+
     </>
   );
 };
